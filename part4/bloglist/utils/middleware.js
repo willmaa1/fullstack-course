04c27361ef85a0240b172aa17e26206e7bfac692
Alex Willman <alex.willman@aalto.fi>
2023-06-12 20:28:36 +0300

    Fix typo in error handler middleware

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -14,7 +14,7 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.name === "JsonWebTokenError") {
     return response.status(400).json({ error: error.message })
   } else if (error.name === "TokenExpiredError") {
-    return response.statur(401).json({ error: "token expired" })
+    return response.status(401).json({ error: "token expired" })
   }
   next(error)
 }
